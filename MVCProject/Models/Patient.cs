3b6e697c2ee75938e660b4dafc7512c741541669
anastasia-sl <anastasiaslynko2206@gmail.com>
2024-05-02 17:02:29 +0300

    Fixed code

--- MVCProject/Models/Patient.cs
+++ MVCProject/Models/Patient.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +9,10 @@ namespace MVCProject.Models
 {
     public class Patient
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
         public string Name { get; set; }
     }
 }
